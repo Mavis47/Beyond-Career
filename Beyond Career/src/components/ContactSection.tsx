b449cdc8f6a9580fd31587144a6285a7b3a2785f
manish vishwakarma <prakashvish1973@gmail.com>
2025-06-30 12:41:46 +0530

    Zod validation added

--- Beyond Career/src/components/ContactSection.tsx
+++ Beyond Career/src/components/ContactSection.tsx
@@ -1,6 +1,38 @@
+import { useState } from 'react';
 import '../styles/contact.css';
+import { z } from "zod";
+import {toast} from "react-toastify";
+
+const schema = z.object({
+  name: z.string().min(1,"Name is required"),
+  email: z.string().min(1,"Email is required"),
+  message: z.string().min(1,"Message is required"),
+});
 
 export default function ContactSection() {
+
+  const [form,setform] = useState({name: "",message: "",email: ""});
+
+  // Form validation using zod
+  const handleInputs = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    e.preventDefault();
+    setform({...form,[e.target.value]: e.target.value});
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const result = schema.safeParse(form);
+
+    if(!result.success){
+      toast.error(result.error.errors[0].message);
+      return;
+    }
+
+    toast.success("Message sent successfully!");
+    setform({ name: "", email: "", message: "" }); //Resetting form
+  }
+
+
   return (
     <section className="contact-section" id="contact">
       <h2>Contact Us</h2>
@@ -8,10 +40,10 @@ export default function ContactSection() {
 
       <div className="contact-grid">
         {/* Contact Form */}
-        <form className="contact-form">
-          <input type="text" placeholder="Your Name" required />
-          <input type="email" placeholder="Your Email" required />
-          <textarea placeholder="Your Message"  required></textarea>
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Your Name" value={form.name} required onChange={handleInputs}/>
+          <input type="email" placeholder="Your Email" value={form.email} required onChange={handleInputs}/>
+          <textarea placeholder="Your Message"  required value={form.message} onChange={handleInputs}></textarea>
           <button type="submit">Send Message</button>
         </form>
 
