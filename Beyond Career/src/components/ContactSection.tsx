f6c056e8205bfb2c832c9221549cf9bb7c70b9c7
manish vishwakarma <prakashvish1973@gmail.com>
2025-06-30 12:51:08 +0530

    Name & Email validation added

--- Beyond Career/src/components/ContactSection.tsx
+++ Beyond Career/src/components/ContactSection.tsx
@@ -4,8 +4,8 @@ import { z } from "zod";
 import {toast} from "react-toastify";
 
 const schema = z.object({
-  name: z.string().min(1,"Name is required"),
-  email: z.string().min(1,"Email is required"),
+  name: z.string().min(1,"Name is required").regex(/^[A-Za-z\s]+$/, "Name should contain only letters and spaces"),
+  email: z.string().min(1,"Email is required").email("Enter a valid email address"),
   message: z.string().min(1,"Message is required"),
 });
 
