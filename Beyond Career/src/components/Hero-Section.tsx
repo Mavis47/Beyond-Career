8bba785853ed1e4e74d178e464ce7d269b66c51b
manish vishwakarma <prakashvish1973@gmail.com>
2025-06-30 13:09:26 +0530

    added spinner

--- Beyond Career/src/components/Hero-Section.tsx
+++ Beyond Career/src/components/Hero-Section.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "../styles/Hero-Section.css";
+import { ClipLoader } from "react-spinners"; 
 
 export default function HeroSection() {
+
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+
   return (
     <div className="hero-section" id="home">
        
+      {!isVideoLoaded && (
+        <div className="video-loader">
+          <ClipLoader size={50} color="#ffffff" loading />
+        </div>
+      )}
+
       <div className="hero-content">
         <h1 className="heading-one">Redefining Career Journeys for Students</h1>
         <p className="paragraph-header">At Beyond Career, we're bridging the gap between ambition and opportunity.Get expert and —   <br />
@@ -18,7 +30,7 @@ export default function HeroSection() {
         <button className="cta-button">Get Started</button>
       </div>
       <div className="hero-background">
-        <video autoPlay muted loop playsInline className="hero-video">
+        <video autoPlay muted loop playsInline className="hero-video" onLoadedData={() => setIsVideoLoaded(true)}>
           <source src="/assets/hero.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
